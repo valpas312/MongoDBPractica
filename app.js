f19af7d44046f01ecfa8f8b6056a9df31ead3155
valpas312 <96994527+valpas312@users.noreply.github.com>
2023-06-10 19:49:47 -0300

    conexion basica con mongodb a una base de datos

---
+++ app.js
@@ -0,0 +1,17 @@
+import { URL } from "./constantes";
+import { MongoClient } from "mongodb";
+
+async function main() {
+    const client = new MongoClient(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+    } catch (err){
+        console.log(err);
+    } finally {
+        await client.close();
+        console.log("Disconnected correctly from server")
+    }
+}
+
+main().catch(console.error);
---
+++ constantes.js
@@ -0,0 +1,4 @@
+const URL = "mongodb+srv://valpas312:41667229Vv@cluster0.naob6w4.mongodb.net/"
+const DB = "practice"
+
+export { URL, DB }
